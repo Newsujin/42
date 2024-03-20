3987ec569b7165d7624484e0aba3d7f153868f5f
sujin <sujin@Mac.local>
2024-03-20 21:58:42 +0900

    cpp ex00

--- 4/cpp00/ex00/megaphone.cpp
+++ 4/cpp00/ex00/megaphone.cpp
@@ -2,14 +2,16 @@
 
 int main(int ac, char *av[])
 {
+
     if (ac == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
     {
         for (int i = 1; i < ac; i++)
         {
-            for (int j = 0; av[i][j]; j++)
-                std::cout << static_cast<char>(std::toupper(av[i][j]));
+            std::string str(av[i]);
+            for (int j = 0; str[j]; j++)
+                std::cout << static_cast<char>(std::toupper(str[j]));
         }
     }
     std::cout << std::endl;
