90f690d72bab0af1b6d292a36b83d217854263d8
sujin Park <chemd78@naver.com>
2024-03-27 20:56:44 +0900

    chore: cpp00 phonebook이 비었을 경우 search 출력문 수정

--- 4/cpp00/ex01/src/phonebook.cpp
+++ 4/cpp00/ex01/src/phonebook.cpp
@@ -9,33 +9,34 @@ void    PhoneBook::Add(void)
     this->contacts[idx].setNickName();
     this->contacts[idx].setPhoneNumber();
     this->contacts[idx].setDarkestSecret();
-    std::cout << "Saved successfully" << std::endl;
+    std::cout << "Saved successfully *^^*" << std::endl << std::endl;
     this->idx = ++idx % 8;
     size += (size < 8);
 }
 
 void    PhoneBook::Search()
 {
-    std::cout << "---------------------------------------------" << std::endl;
-    std::cout << "|" << std::setw(10) << "index"
-            << "|" << std::setw(10) << "first name"
-            << "|" << std::setw(10) << "last name"
-            << "|" << std::setw(10) << "nick name"
-            << "|" << std::endl;
-    std::cout << "---------------------------------------------" << std::endl;
-
-    for (int i = 0; i < this->size; i++)
-    {
-        std::string first_name = this->contacts[i].getFirstName();
-        std::string last_name = this->contacts[i].getLastName();
-        std::string nick_name = this->contacts[i].getNickName();
-        std::cout << "|" << std::setw(10) << i
-                << "|" << std::setw(10) << GetTenStr(first_name)
-                << "|" << std::setw(10) << GetTenStr(last_name)
-                << "|" << std::setw(10) << GetTenStr(nick_name)
-                << "|" << std::endl;
-    }
     if (this->size)
+    {
+        std::cout << "---------------------------------------------" << std::endl;
+        std::cout << "|" << std::setw(10) << "index"
+                << "|" << std::setw(10) << "first name"
+                << "|" << std::setw(10) << "last name"
+                << "|" << std::setw(10) << "nick name"
+                << "|" << std::endl;
+        std::cout << "---------------------------------------------" << std::endl;
+
+        for (int i = 0; i < this->size; i++)
+        {
+            std::string first_name = this->contacts[i].getFirstName();
+            std::string last_name = this->contacts[i].getLastName();
+            std::string nick_name = this->contacts[i].getNickName();
+            std::cout << "|" << std::setw(10) << i
+                    << "|" << std::setw(10) << GetTenStr(first_name)
+                    << "|" << std::setw(10) << GetTenStr(last_name)
+                    << "|" << std::setw(10) << GetTenStr(nick_name)
+                    << "|" << std::endl;
+        }
         std::cout << "---------------------------------------------" << std::endl;
     
     std::string index;
@@ -47,17 +48,20 @@ void    PhoneBook::Search()
         std::cout << std::endl << "That's wrong index!" << std::endl;
     else
         PrintContact(index[0] - '0');
+    }
+    else
+        std::cout << std::endl << "phonebook is empty!" << std::endl;
 }
 
 void    PhoneBook::PrintContact(int index)
 {
-    std::cout << std::endl << "--------- this is user information. ---------" << std::endl;
+    std::cout << std::endl << "------------ Contact Information ------------" << std::endl;
     std::cout << "first name: " << this->contacts[index].getFirstName() << std::endl;
     std::cout << "last name: " << this->contacts[index].getLastName() << std::endl;
     std::cout << "nick name: " << this->contacts[index].getNickName() << std::endl;
     std::cout << "phone number: " << this->contacts[index].getPhoneNumber() << std::endl;
     std::cout << "darkest secret: " << this->contacts[index].getDarkestSecret() << std::endl;
-    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "---------------------------------------------" << std::endl << std::endl;
 }
 
 std::string PhoneBook::GetTenStr(std::string str)
