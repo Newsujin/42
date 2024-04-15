cc65f86907954d9a979bb837d74525ebe3de1b22
sujin Park <chemd78@naver.com>
2024-04-15 20:44:14 +0900

    chore: cpp01

--- 4/cpp01/ex00/Zombie.cpp
+++ 4/cpp01/ex00/Zombie.cpp
@@ -4,7 +4,7 @@ Zombie::Zombie(std::string name) {this->name = name;}
 
 Zombie::~Zombie(void)
 {
-    std::cout << this->name << "is dead" << std::endl;
+    std::cout << this->name << " is dead" << std::endl;
 }
 
 void    Zombie::announce(void)
