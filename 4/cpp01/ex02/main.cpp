83425989ae77d14f67cf1cd44be2a2c98a1cd5c0
Newsujin <chemd78@naver.com>
2024-04-16 19:21:27 +0900

    add: cpp01 ex02

--- 4/cpp01/ex02/main.cpp
+++ 4/cpp01/ex02/main.cpp
@@ -2,7 +2,19 @@
 
 int main(void)
 {
-    std::string str("HI THIS IS BRAIN");
-    std::cout << str << std::endl;
-    return (0);
+	std::string str("HI THIS IS BRAIN");
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "address" << std::endl;
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << "value" << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	return (0);
 }
