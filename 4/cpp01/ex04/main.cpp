287a7881b8cc378e0801444d8eadce0450f0d77c
sujin Park <chemd78@naver.com>
2024-04-22 15:31:38 +0900

    add: cpp01 ex04

---
+++ 4/cpp01/ex04/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+
+int main(int ac, char **av)
+{
+    if (ac != 4)
+        return (std::cout << "argc error" << std::endl, 1);
+
+    if (!std::strlen(av[1]) || !std::strlen(av[2]))
+        return (std::cout << "argv length error" << std::endl, 1);
+
+    std::string infile = av[1];
+    std::ifstream ifs(infile);
+    if (ifs.fail())
+        return (std::cout << infile << " open error" << std::endl, 1);
+
+    std::string outfile = infile + ".replace";
+    std::ofstream ofs(outfile);
+    if (ofs.fail())
+        return (std::cout << outfile << " open error" << std::endl, 1);
+   
+    std::string s1 = av[2];
+    std::string s2 = av[3];
+    int len1 = s1.length();
+    int len2 = s2.length();
+    std::string line;
+    while (1)
+    {
+        std::getline(ifs, line);
+        size_t pos = 0;
+        while (1)
+        {
+            pos = line.find(s1, pos);
+            if (pos == std::string::npos)
+                break;
+            line.erase(pos, len1);
+            line.insert(pos, s2);
+            pos += len2;
+        }
+        ofs << line;
+        if (ifs.eof())
+            break;
+        ofs << std::endl;
+    }
+    ifs.close();
+    ofs.close();
+    return 0;
+}
