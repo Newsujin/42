8139b8221e3e3eec11e3c8e8de32f10d5e164cb6
sujin Park <chemd78@naver.com>
2024-04-23 16:46:13 +0900

    chore: cpp01 ex05

--- 4/cpp01/ex05/Harl.hpp
+++ 4/cpp01/ex05/Harl.hpp
@@ -4,7 +4,13 @@
 # include <iostream>
 
 class Harl {
-    
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    public:
+        void complain(std::string level);
 };
 
 #endif
