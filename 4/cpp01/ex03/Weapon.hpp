6a2b8d919a99c450607f75605ab81b7c14491a5b
sujin Park <chemd78@naver.com>
2024-04-25 13:20:52 +0900

    chore: reference 테스트

--- 4/cpp01/ex03/Weapon.hpp
+++ 4/cpp01/ex03/Weapon.hpp
@@ -9,8 +9,7 @@ class Weapon {
     public:
         Weapon(std::string type);
         ~Weapon();
-        const std::string &getType(); //함수 뒤에 const를 꼭 붙여야 하는지
-        // const std::string getType(); //레퍼런스가 아닌 경우에는 왜 human의 이름이 바뀌는지
+        const std::string &getType() const;
         void setType(std::string type);
 };
 
