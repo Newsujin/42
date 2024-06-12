2b17eadc7cfdbc7e5f13e65bcfa2742945a945cf
Newsujin <chemd78@naver.com>
2024-06-12 12:47:11 +0900

    chore: optimization

--- 5/cpp06/ex00/Utils.cpp
+++ 5/cpp06/ex00/Utils.cpp
@@ -1,28 +1,20 @@
 #include "ScalarConverter.hpp"
 
 int ScalarConverter::checkType(const std::string& literal) {
-	if (literal == "nanf" || literal == "+inff" || literal == "-inff") return FLOAT;
-	if (literal == "nan" || literal == "+inf" || literal == "-inf") return DOUBLE;
-
-	// Check if it is a single character
 	if (literal.length() == 1 && !isdigit(literal[0])) return CHAR;
 
-	char* end;
 	errno = 0;
-
-	// Try converting to int
+	char* end;
 	long l = std::strtol(literal.c_str(), &end, 10);
-	if (*end == '\0' && errno != ERANGE &&
-		l >= std::numeric_limits<int>::min() && l <= std::numeric_limits<int>::max()) {
-		return INT;
-	}
 
-	// Try converting to float
+	if (*end == '\0' && errno != ERANGE &&
+		l >= std::numeric_limits<int>::min() && l <= std::numeric_limits<int>::max())
+		return INT;
+
 	errno = 0;
 	std::strtof(literal.c_str(), &end);
 	if ((*end == 'f' || *end == 'F') && *(end + 1) == '\0' && errno != ERANGE) return FLOAT;
 
-	// Try converting to double
 	errno = 0;
 	std::strtod(literal.c_str(), &end);
 	if (*end == '\0' && errno != ERANGE) return DOUBLE;
@@ -30,12 +22,10 @@ int ScalarConverter::checkType(const std::string& literal) {
 	return STR_ERROR;
 }
 
-// 잘못된 형식일 경우
 void ScalarConverter::strError(const std::string& literal) {
 	std::cout << "str error: \"" << literal << "\"\n";
 }
 
-// 범위를 벗어날 경우
 void ScalarConverter::sizeError(const std::string& literal) {
 	std::cout << "Invalid Type: \"" << literal << "\"\n";
 }
