0b81ccbd7336fb8b5cd07202033b0e2d123c97e2
Newsujin <chemd78@naver.com>
2024-06-19 13:50:04 +0900

    chore: cpp07 main

--- 5/cpp07/ex00/main.cpp
+++ 5/cpp07/ex00/main.cpp
@@ -3,7 +3,7 @@
 int main( void ) {
     int a = 2;
     int b = 3;
-    
+
     ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -16,6 +16,6 @@ int main( void ) {
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    
+
     return 0;
 }
