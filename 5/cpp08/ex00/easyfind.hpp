97388479d426de3a3d6767a7e1ef18d8eda7c921
Newsujin <chemd78@naver.com>
2024-06-20 16:18:29 +0900

    add: cpp08 ex00

--- 5/cpp08/ex00/easyfind.hpp
+++ 5/cpp08/ex00/easyfind.hpp
@@ -1,6 +1,24 @@
-#ifndef EASHFIND_HPP
-# define EASHFIND_HPP
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <vector>
+# include <list>
+
+class NotFoundException : public std::exception {
+	public:
+		const char* what() const throw() {
+			return ("element not found!");
+		}
+};
 
 template <typename T>
+typename T::iterator easyfind(T& container, int value) {
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw NotFoundException();
+	return (it);
+}
 
 #endif
---
+++ 5/cpp08/ex00/main.cpp
@@ -0,0 +1,33 @@
+#include "easyfind.hpp"
+
+int main() {
+	try {
+		std::vector<int> vec;
+		for (int i = 0; i < 10; i++)
+			vec.push_back(i);
+
+		std::vector<int>::iterator it;
+		it = easyfind(vec, 3);
+		// it = easyfind(vec, 100);
+		std::cout << *it << std::endl;
+	}
+	catch (const NotFoundException& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		std::list<int> lst;
+		for (int i = 0; i < 10; i++)
+			lst.push_back(i);
+
+		std::list<int>::iterator it;
+		it = easyfind(lst, 5);
+		// it = easyfind(lst, 100);
+		std::cout << *it << std::endl;
+	}
+	catch (const NotFoundException& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
+}
