da2b04f90d45c2245bb0d86509191c06fa3f2ec4
Newsujin <chemd78@naver.com>
2024-06-25 19:17:56 +0900

    chore: arr 출력 한번으로 수정

--- 5/cpp09/ex02 copy/main.cpp
+++ 5/cpp09/ex02 copy/main.cpp
@@ -9,18 +9,16 @@ int	main(int ac, char **av) {
 	try {
 		PmergeMe pmm(ac, av);
 
-		pmm.printVec(BEFORE_SORT);
+		pmm.printArr(BEFORE_SORT);
 		clock_t vec_t = clock();
 		pmm.fordJohnsonVec();
         vec_t = clock() - vec_t;
-		pmm.printVec(AFTER_SORT);
+		pmm.printArr(AFTER_SORT);
         std::cout << "Time to process a range of " << ac - 1 << " element with std::vector : " << static_cast<float>(vec_t) * 1000000 / CLOCKS_PER_SEC << " us" << std::endl;
 
 		clock_t deq_t = clock();
-		pmm.printDeq(BEFORE_SORT);
 		pmm.fordJohnsonDeq();
         deq_t = clock() - deq_t;
-		pmm.printDeq(AFTER_SORT);
         std::cout << "Time to process a range of " << ac - 1 << " element with std::deque : " << static_cast<float>(deq_t) * 1000000 / CLOCKS_PER_SEC << " us" << std::endl;
 
 	}
